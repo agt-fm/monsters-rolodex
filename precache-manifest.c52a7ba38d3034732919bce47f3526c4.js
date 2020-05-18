99fbc8a00e1bc4e6f436eb63ecc6207e07ce0ae8
Andrey <a.g.terentyev@gmail.com>
2020-05-18 15:35:47 +0300

    Updates

---
+++ precache-manifest.c52a7ba38d3034732919bce47f3526c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912067be0a400341aa6c820e382b7ea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dbc31e00a0c94fc96ce",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "df3556b13584b886dd46",
+    "url": "/static/js/2.4392d1ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4392d1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbc31e00a0c94fc96ce",
+    "url": "/static/js/main.1bb5044a.chunk.js"
+  },
+  {
+    "revision": "0b26c044026ecf183e79",
+    "url": "/static/js/runtime-main.464fd773.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
